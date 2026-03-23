9ee9fdb5012fb45138ec97e8a81e03225dbd2b94
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2026-03-24 02:26:29 +0400

    -

--- src/Security/Module.php
+++ src/Security/Module.php
@@ -76,10 +76,10 @@ class Module extends BaseModule
             session_set_cookie_params(["Secure" => "true"]); //false, true        
         }
         
-        @session_start();
+        session_start();
         $this->_id = isset($_SESSION['SS_MEMBER']) ? $_SESSION['SS_MEMBER'] : null;
         $this->_hash = isset($_SESSION['SS_HASH']) ? $_SESSION['SS_HASH'] : null;
-        @session_write_close();
+        session_write_close();
 
         if (!$this->_id) {
             return;
@@ -108,10 +108,10 @@ class Module extends BaseModule
             session_set_cookie_params(["Secure" => "true"]); //false, true        
         }
 
-        @session_start();
+        session_start();
         $_SESSION['SS_MEMBER'] = $this->_id;
         $_SESSION['SS_HASH'] = $this->_hash;
-        @session_write_close();
+        session_write_close();
 
     }
 
@@ -121,10 +121,10 @@ class Module extends BaseModule
             session_set_cookie_params(["SameSite" => "None"]); //none, lax, strict
             session_set_cookie_params(["Secure" => "true"]); //false, true        
         }
-        @session_start();
+        session_start();
         $_SESSION['SS_MEMBER'] = null;
         $_SESSION['SS_HASH'] = null;
-        @session_write_close();
+        session_write_close();
 
         $this->_id = null;
         $this->_hash = null;
